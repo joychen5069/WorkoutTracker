3956f790edde256451f3195a3ef43dd756ca76a6
Joy Chen <joychen5069@gmail.com>
2020-06-18 15:27:57 -0400

    Fixed stats html path

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,6 +6,10 @@ module.exports = (app) => {
         res.sendFile(path.join(__dirname, '../public/exercise.html'));
     });
 
+    app.get('/stats', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/stats.html'));
+    });
+
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/index.html'));
     });
