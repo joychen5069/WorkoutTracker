dc9eaece7ec7a85dcf867f38190540271068a780
Joy Chen <joychen5069@gmail.com>
2020-06-18 15:25:14 -0400

    got new workout to go to exercise.html

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,9 +2,9 @@ const path = require("path");
 
 //create file paths for html
 module.exports = (app) => {
-    // app.get('/exercise', (req, res) => {
-    //     res.sendFile(path.join(__dirname, '../public/exercise.html'));
-    // });
+    app.get('/workouts', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/exercise.html'));
+    });
 
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/index.html'));
