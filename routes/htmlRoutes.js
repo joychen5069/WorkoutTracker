3e7ba3395cbac0d162f285090702296b8fb229ec
Joy Chen <joychen5069@gmail.com>
2020-06-18 15:21:45 -0400

    have index working

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,12 +1,12 @@
 const path = require("path");
 
 //create file paths for html
-module.exports = app => {
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(_dirname, '..public/index.html'));
-    })
+module.exports = (app) => {
+    // app.get('/exercise', (req, res) => {
+    //     res.sendFile(path.join(__dirname, '../public/exercise.html'));
+    // });
 
-    app.get('/workouts', (req, res) => {
-        res.sendFile(path.join(_dirname, '..public/exercise.html'));
-    })
-}
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
+};
