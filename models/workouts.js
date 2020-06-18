2035dc37c27b2e458de3ea5d20acb56740ccc5e4
Joy Chen <joychen5069@gmail.com>
2020-06-18 15:41:40 -0400

    added workout schema

---
+++ models/workouts.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    //set date for exercises
+    day: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    exercises: [{
+        type: {
+            type: String,
+            required: "Enter exercise"
+        },
+
+        name: {
+            type: String,
+            required: "Enter the name of the exercise"
+        },
+
+        duration: {
+            type: Number,
+            required: "Enter the duration of the exercise"
+        },
+
+        weight: {
+            type: Number,
+            required: "Enter the weight"
+        },
+
+        reps: {
+            type: Number,
+            required: "Enter the amount of reps"
+        },
+
+        sets: {
+            type: Number,
+            required: "Enter the amount of sets"
+        }
+    }]
+})
+
+
+const Transaction = mongoose.model("workouts", workoutSchema);
+
+module.exports = Transaction;
