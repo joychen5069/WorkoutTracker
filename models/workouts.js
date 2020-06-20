a85ddc313fa0fddd0378ddcd3c048665d7559da4
Joy Chen <joychen5069@gmail.com>
2020-06-20 09:36:57 -0400

    created index.js for model and updated schema for mongoose

--- models/workouts.js
+++ models/workouts.js
@@ -3,46 +3,37 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-    //set date for exercises
-    day: {
-        type: Date,
-        required: true,
-        default: Date.now
+  //create schema for mongoose
+  day: {
+    type: Date, 
+    default: Date.now
+  },
+  exercises: {
+    type: {
+        type: String,
+        trim: true,
     },
-    exercises: [{
-        type: {
-            type: String,
-            required: "Enter exercise"
-        },
+    name: {
+        type: String,
+        trim: true,
+    },
+    duration: {
+        type: Number,
+    },
+    weight: {
+        type: Number,
+    },
+    reps: {
+        type: Number,
+    },
+    sets: {
+        type: Number,
+    },
+},
 
-        name: {
-            type: String,
-            required: "Enter the name of the exercise"
-        },
-
-        duration: {
-            type: Number,
-            required: "Enter the duration of the exercise"
-        },
-
-        weight: {
-            type: Number,
-            required: "Enter the weight"
-        },
-
-        reps: {
-            type: Number,
-            required: "Enter the amount of reps"
-        },
-
-        sets: {
-            type: Number,
-            required: "Enter the amount of sets"
-        }
-    }]
-})
-
+});
 
 const Workout = mongoose.model("workouts", workoutSchema);
 
 module.exports = Workout;
+
