23a1a14df6b38a2d0055e04cbe6c7a54787d2c82
Joy Chen <joychen5069@gmail.com>
2020-06-20 09:44:11 -0400

    fixed server.js to connect to mongoose correctly

--- server.js
+++ server.js
@@ -1,28 +1,32 @@
-const express = require("express");
 const mongoose = require("mongoose");
-// const Workout = require("./models/workouts");
-
-const PORT = process.env.PORT || 3000
+const path = require('path');
+const express = require("express");
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+const PORT = process.env.PORT || 3000;
+
+const workout = require("./models/workouts.js");
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 const db = mongoose.connection;
-db.once('open', () => console.log("connected to mongo"));
 db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("database connected");
+});
 
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app); 
+
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+
