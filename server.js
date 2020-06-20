79271a70cfdcacce20eb13c4808b460547d96c98
Joy Chen <joychen5069@gmail.com>
2020-06-20 10:00:41 -0400

    fixed formatting and added db for heroku

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.use(express.json());
 app.use(express.static('public'));
 
 const PORT = process.env.PORT || 3000;
+//for heroku?
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
