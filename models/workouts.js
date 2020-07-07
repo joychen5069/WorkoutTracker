8e00fe013a9ff50a726c142ac4fa249ce0df8956
Joy Chen <joychen5069@gmail.com>
2020-07-06 20:37:33 -0400

    updated model to display cardio on main page

--- models/workouts.js
+++ models/workouts.js
@@ -1,39 +1,55 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
-
-const workoutSchema = new Schema({
-    //create schema for mongoose
+const workoutSchema = new Schema(
+  {
     day: {
-        type: Date,
-        default: Date.now
+      type: Date,
+      default: () => new Date()
     },
-    exercises: {
+    exercises: [
+      {
         type: {
-            type: String,
-            trim: true,
+          type: String,
+          trim: true,
+          required: "Enter an exercise type"
         },
         name: {
-            type: String,
-            trim: true,
+          type: String,
+          trim: true,
+          required: "Enter an exercise name"
         },
         duration: {
-            type: Number,
+          type: Number,
+          required: "Enter an exercise duration in minutes"
         },
         weight: {
-            type: Number,
+          type: Number
         },
         reps: {
-            type: Number,
+          type: Number
         },
         sets: {
-            type: Number,
+          type: Number
         },
-    },
-
+        distance: {
+          type: Number
+        }
+      }
+    ]
+  },
+  {
+    toJSON: {
+      // include any virtual properties when data is requested
+      virtuals: true
+    }
+  }
+);
+// adds a dynamically-created property to schema
+workoutSchema.virtual("totalDuration").get(function () {
+  // "reduce" array of exercises down to just the sum of their durations
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
 });
-
-const Workout = mongoose.model("workouts", workoutSchema);
-
+const Workout = mongoose.model("Workout", workoutSchema);
 module.exports = Workout;
-
